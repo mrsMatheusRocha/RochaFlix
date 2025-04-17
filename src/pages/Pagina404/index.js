f2dd99a74d0056a79b9ea8dc04407c4ea8d27417
Matheus Rocha Sousa <166670004+Marthplays@users.noreply.github.com>
2025-04-17 15:36:02 -0300

    Add FlappyBird game to error 404

--- src/pages/Pagina404/index.js
+++ src/pages/Pagina404/index.js
@@ -1,13 +1,35 @@
 import React from "react";
+import PageDefault from "../../components/PageDefault";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Texts = styled.div`
+  text-align: center;
+`;
 
 function Pagina404() {
   return (
-    <div>
-  <p>
-    ddadsd
-  </p>
-    </div>
-  );
+    <PageDefault>
+      <Texts>
+        <h1>Error 404</h1>
+        <Link to="/">
+          Voltar para a Home
+        </Link>
+        <h2>Volte para a Home ou jogue o joguinho abaixo xD</h2>
+      </Texts>
+      <iframe
+        title="Flappy Bird Game"
+        src="https://mrsmatheusrocha.github.io/FlappyBird/"
+        width="340"
+        height="475" 
+        style={{
+          display: "flex",
+          margin: "0 auto",
+          marginBottom: "20px"
+        }}
+      />
+    </PageDefault>
+  )
 }
 
 export default Pagina404;
